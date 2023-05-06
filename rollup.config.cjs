bd231673a82866a58a3822fcd95e036bdf6bb481
malkiAbdoo <abdo.malkiep@gmail.com>
2023-05-06 17:42:02 +0100

    refactor(config): rewrite the rollup bundle code

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,10 +1,12 @@
 import { readFileSync } from 'fs';
-import { terser } from 'rollup-plugin-terser';
-import typescript from '@rollup/plugin-typescript';
 import * as meta from './package.json';
+import { terser } from 'rollup-plugin-terser';
+import deletePlugin from 'rollup-plugin-delete';
+import typescript from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts';
 
 // Remove the scope from the package name
-const packageName = meta.name.replace(/@.+\//g, '');
+const name = meta.name.replace(/@.+\//g, '');
 
 // Extract copyrights from the LICENSE
 const copyright = readFileSync('./LICENSE', 'utf-8')
@@ -13,43 +15,46 @@ const copyright = readFileSync('./LICENSE', 'utf-8')
   .map(line => line.replace(/^Copyright\s+/, ''))
   .join(', ');
 
-const config = {
+const banner = `// ${meta.homepage} v${meta.version} Copyright ${copyright}`;
+
+const globals = Object.assign(
+  {},
+  ...Object.keys(meta.dependencies || {})
+    .filter(key => /^d3-/.test(key))
+    .map(key => ({ [key]: 'd3' }))
+);
+
+const bundle = config => ({
+  ...config,
   input: 'src/index.ts',
-  external: Object.keys(meta.dependencies || {}).filter(key =>
-    /^d3-/.test(key)
-  ),
-  output: {
-    file: `dist/${packageName}.js`,
-    name: 'd3',
-    format: 'umd',
-    indent: false,
-    extend: true,
-    banner: `// ${meta.homepage} v${meta.version} Copyright ${copyright}`,
-    globals: Object.assign(
-      {},
-      ...Object.keys(meta.dependencies || {})
-        .filter(key => /^d3-/.test(key))
-        .map(key => ({ [key]: 'd3' }))
-    )
-  },
-  plugins: [typescript()]
-};
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key))
+});
 
 export default [
-  config,
-  {
-    ...config,
-    output: {
-      ...config.output,
-      file: `dist/${packageName}.min.js`
-    },
-    plugins: [
-      ...config.plugins,
-      terser({
-        output: {
-          preamble: config.output.banner
-        }
-      })
+  bundle({
+    plugins: [typescript()],
+    output: [
+      {
+        name,
+        file: `dist/${name}.js`,
+        format: 'es',
+        banner,
+        globals
+      },
+      {
+        name,
+        file: `dist/${name}.min.js`,
+        format: 'umd',
+        globals,
+        plugins: [terser({ output: { preamble: banner } })]
+      }
     ]
-  }
+  }),
+  bundle({
+    plugins: [dts.default(), deletePlugin({ targets: 'dist/!(index).d.ts' })],
+    output: {
+      file: `dist/index.d.ts`,
+      format: 'es'
+    }
+  })
 ];
